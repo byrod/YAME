0ce3d8893ca8a3a10cf3405fc28f95dd9ca4209e
byrod <webmaster@byrod.fr>
2016-02-14 12:48:05 +0100

    SmartDialog

---
+++ YAME/SMARTDIALOG/SmartDialog.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace YAME.SMARTDIALOG
+{
+    partial class SmartDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.rText = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(93, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(455, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::YAME.Properties.Resources.YAMEv2_transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(4, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(63, 63);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.LightGray;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Location = new System.Drawing.Point(18, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightGray;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(12, 9);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(72, 170);
+            this.panel2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(378, 185);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(62, 28);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // rText
+            // 
+            this.rText.BackColor = System.Drawing.Color.White;
+            this.rText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rText.Cursor = System.Windows.Forms.Cursors.Default;
+            this.rText.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rText.Location = new System.Drawing.Point(18, 19);
+            this.rText.Name = "rText";
+            this.rText.ReadOnly = true;
+            this.rText.Size = new System.Drawing.Size(393, 132);
+            this.rText.TabIndex = 4;
+            this.rText.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.rText);
+            this.panel1.Font = new System.Drawing.Font("Century Gothic", 8.25F);
+            this.panel1.Location = new System.Drawing.Point(90, 9);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(427, 170);
+            this.panel1.TabIndex = 0;
+            // 
+            // SmartDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 216);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SmartDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SmartDialog";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox rText;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ YAME/SMARTDIALOG/SmartDialog.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace YAME.SMARTDIALOG
+{
+    public partial class SmartDialog : Form
+    {
+        public SmartDialog()
+        {
+            InitializeComponent();
+        }
+        static SmartDialog MsgBox;
+        static DialogResult result = new DialogResult();
+
+        public static DialogResult Show(string message, string label2, string label1, string Caption, string BtnOk, string BtnCancel = "")
+        {
+            MsgBox = new SmartDialog();
+            MsgBox.rText.Text = message;
+            MsgBox.rText.SelectAll();
+            MsgBox.rText.SelectionAlignment = HorizontalAlignment.Center;
+            MsgBox.rText.Select(0, 0);
+
+            MsgBox.label1.Text = label1;
+            MsgBox.label2.Text = label2;
+            MsgBox.Text = Caption;
+
+            MsgBox.button1.Text = BtnOk;
+            if (BtnCancel == "")
+                MsgBox.button2.Hide();
+            else
+                MsgBox.button2.Text = BtnCancel;
+
+            MsgBox.ShowDialog();
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            result = DialogResult.Yes;
+            MsgBox.Close();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            result = DialogResult.No;
+            MsgBox.Close();
+        }
+
+    }
+}
